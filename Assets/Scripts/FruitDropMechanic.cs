6ad9f48b4361f2cb6396b29f7dbc2f3d9a06b174
ZanY3 <146362968+ZanY3@users.noreply.github.com>
2024-03-20 19:20:22 +0200

    Visual Fruits show(next fruit )

--- Assets/Scripts/FruitDropMechanic.cs
+++ Assets/Scripts/FruitDropMechanic.cs
@@ -7,11 +7,13 @@ public class FruitDropMechanic : MonoBehaviour
 {
     [Header("Fruits")]
     public List<GameObject> fruitPrefabs;
+    public List<GameObject> visualFruits;
     public bool isReadyToReplace = false; //merge fruit
     public int whichFruit = 0;
     public Vector2 spawnPos;
     public Transform visualWhichFruitPos;
     public float throwCooldown;
+    private GameObject randomFruit;
     [Header("Sounds")]
     public AudioSource source;
     public AudioClip spawnSound;
@@ -19,12 +21,97 @@ public class FruitDropMechanic : MonoBehaviour
     [Header("Score")]
     public int scoreCount = 1;
 
+
+    void Start()
+    {
+        randomFruit = fruitPrefabs[Random.Range(0, 5)];
+        if (randomFruit.tag == "0")
+        {
+            visualFruits[0].gameObject.SetActive(true);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "1")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(true);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "2")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(true);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "3")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(true);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "4")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(true);
+        }
+    }
     void Update()
     {
+        if (randomFruit.tag == "0")
+        {
+            visualFruits[0].gameObject.SetActive(true);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "1")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(true);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "2")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(true);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "3")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(true);
+            visualFruits[4].gameObject.SetActive(false);
+        }
+        if (randomFruit.tag == "4")
+        {
+            visualFruits[0].gameObject.SetActive(false);
+            visualFruits[1].gameObject.SetActive(false);
+            visualFruits[2].gameObject.SetActive(false);
+            visualFruits[3].gameObject.SetActive(false);
+            visualFruits[4].gameObject.SetActive(true);
+        }
+
         throwCooldown -= Time.deltaTime;
         if(Input.GetKeyDown(KeyCode.Mouse0))
         {
-            var randomFruit = fruitPrefabs[Random.Range(0, 5)];
 
             if(throwCooldown <= 0)
             {
@@ -33,7 +120,8 @@ public class FruitDropMechanic : MonoBehaviour
                 var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 worldPos.z = 0;worldPos.y = 4f;
                 Instantiate(randomFruit, worldPos, Quaternion.identity) ;
-
+                
+                randomFruit = fruitPrefabs[Random.Range(0, 5)];
             }
         }
         replaceFruit();
